d3394de5480f0e52b4121d3614ed18a6def84a9c
Brendan Chen <me@bchen.dev>
2025-12-01 16:58:09 -0800

    Add day 1 part 1

--- day01/main.cpp
+++ day01/main.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main() {
     ifstream input("input.txt");
+
+    int current_rotation = 50;
+    int num_times_zero = 0;
+
+    string line;
+    while (getline(input, line)) {
+        int actual_rotation = stoi(line.substr(1)) % 100;
+        if (line.at(0) == 'L') {
+            current_rotation = (100 + current_rotation - actual_rotation) % 100;
+        } else if (line.at(0) == 'R') {
+            current_rotation = (100 + current_rotation - (100 - actual_rotation)) % 100;
+        }
+
+        if (current_rotation == 0) {
+            num_times_zero += 1;
+        }
+    }
+
+    cout << num_times_zero << endl;
     
     return 0;
 }
